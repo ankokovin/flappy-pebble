b344b5c54fea97026c43be156cd9abc418289fb6
Aleksey Kokovin <rycarok@gmail.com>
2024-01-01 22:07:49 +0500

    fix: window name should reflect the name of the game

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,12 @@ fn main() {
     let height = 1024.0;
     let width = 512.0;
     let mut app = App::new();
-    app
-        .add_state::<GameState>()
+    app.add_state::<GameState>()
         .insert_resource(GameSize::new(width, height))
         .add_plugins((
             DefaultPlugins.set(WindowPlugin {
                 primary_window: Some(Window {
-                    title: "Flappy rock :D".to_string(),
+                    title: "Flappy Pebble :D".to_string(),
                     resizable: false,
                     resolution: WindowResolution::new(width, height),
                     ..Default::default()
@@ -31,13 +30,12 @@ fn main() {
             ui::UiPlugin,
         ))
         .add_systems(Startup, spawn_camera);
-    
-    if cfg!(feature = "egui") {
-        app.add_plugins(WorldInspectorPlugin::default());    
-    }
-    
-    app.run();
 
+    if cfg!(feature = "egui") {
+        app.add_plugins(WorldInspectorPlugin::default());
+    }
+
+    app.run();
 }
 
 fn spawn_camera(mut commands: Commands) {
