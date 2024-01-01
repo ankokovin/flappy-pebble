56ee5aa87bd30fe9d67861954d785d2e23b3045d
Aleksey Kokovin <rycarok@gmail.com>
2024-01-01 16:32:08 +0500

    fix: move pebble physics calculation to FixedUpdate

--- src/pebble.rs
+++ src/pebble.rs
@@ -6,21 +6,27 @@ pub struct PebblePlugin {}
 impl Plugin for PebblePlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, spawn_pebble)
-            .add_systems(FixedUpdate, pebble_deccelerate)
-            .add_systems(Update, (player_input, pebble_move));
+            .add_systems(FixedUpdate, pebble_move)
+            .add_systems(Update, (player_input, render_pebble));
     }
 }
 
 #[derive(Debug, Component)]
 pub struct Pebble {
-    velocity: f32
+    velocity: f32,
+    x: f32,
+    y: f32,
 }
 
 const PEBBLE_DEFAULT_VELOCITY: f32 = 400.0;
 
 impl Default for Pebble {
     fn default() -> Self {
-        Self { velocity: PEBBLE_DEFAULT_VELOCITY }
+        Self { 
+            velocity: PEBBLE_DEFAULT_VELOCITY, 
+            x: 0.0,
+            y: 0.0 
+        }
     }
 }
 
@@ -28,7 +34,6 @@ fn spawn_pebble(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn((
         SpriteBundle {
             texture: asset_server.load("pebble.png"),
-            transform: Transform::from_xyz(0.0, 0.0, 1.0),
             sprite: Sprite {
                 flip_x: true,
                 custom_size: Some(Vec2 { x: 90.0, y: 90.0 }),
@@ -42,15 +47,17 @@ fn spawn_pebble(mut commands: Commands, asset_server: Res<AssetServer>) {
 
 const G_FORCE_ACCELERATION: f32 = -400.0;
 
-fn pebble_deccelerate(time: Res<Time<Fixed>>, mut pebble: Query<&mut Pebble>) {
+fn pebble_move(time: Res<Time<Fixed>>, mut pebble: Query<&mut Pebble>) {
     let mut pebble = pebble.get_single_mut().expect("to get a pebble");
+    pebble.y += pebble.velocity * time.delta_seconds() + G_FORCE_ACCELERATION * time.delta_seconds() * time.delta_seconds() / 2.0;
     pebble.velocity += G_FORCE_ACCELERATION * time.delta_seconds();
 }
 
-fn pebble_move(time: Res<Time>, mut transform_pebble: Query<&mut Transform, With<Pebble>>, pebble: Query<&Pebble>) {
+fn render_pebble(mut transform_pebble: Query<&mut Transform, With<Pebble>>, pebble: Query<&Pebble>) {
     let mut transform = transform_pebble.get_single_mut().expect("to get a pebble transform");
     let pebble = pebble.get_single().expect("to get a pebble");
-    transform.translation.y += pebble.velocity * time.delta_seconds();
+    transform.translation.x = pebble.x;
+    transform.translation.y = pebble.y;
 }
 
 fn player_input(
